cf0a8541c70cf854e11ac757fd1f81f215688cb2
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-03 16:51:21 +0700

    Update start.sh

--- Delegate/start.sh
+++ Delegate/start.sh
@@ -9,27 +9,27 @@ DELAY=300 #in secs - how often restart the script
 
 for (( ;; )); do
         echo -e "Get reward from Delegation"
-        echo -e "${PWDDD}\ny\n" | planqd tx distribution withdraw-rewards --from jb --chain-id planq_7070-2  --gas 1000000 --gas-prices 30000000000 --gas-adjustment 1.15
+        echo -e "${PWDDD}\ny\n" | ${PROJECT} tx distribution withdraw-rewards --from ${WALLETNAME} --chain-id ${CHAIN_ID}  --gas ${GAS} --gas-prices ${GAS_PRICE} --gas-adjustment ${GAS_AD}
 for (( timer=12; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=planqd q bank balances plq1923gyajfkyrt7xwh0p7zmfrdnnfrx6gmzhj3qj --output json | jq -r '.balances[] | select(.denom=="'${DENOM}'")' | jq -r .amount)
+BAL=${PROJECT} q bank balances ${DEL_ADDR} --output json | jq -r '.balances[] | select(.denom=="'${DENOM}'")' | jq -r .amount)
 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} $DENOM\n"
         echo -e "Claim rewards\n"
-        echo -e "${PWDDD}\n${PWDDD}\n" | planqd tx distribution withdraw-all-rewards --from jb --chain-id planq_7070-2  --gas 1000000 --gas-prices 30000000000 --gas-adjustment 1.15
+        echo -e "${PWDDD}\n${PWDDD}\n" | ${PROJECT} tx distribution withdraw-all-rewards --from ${WALLETNAME} --chain-id ${CHAIN_ID}  --gas ${GAS} --gas-prices ${GAS_PRICE} --gas-adjustment ${GAS_AD}
 for (( timer=12; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=planqd q bank balances plq1923gyajfkyrt7xwh0p7zmfrdnnfrx6gmzhj3qj --output json | jq -r '.balances[] | select(.denom=="'${DENOM}'")' | jq -r .amount)
+BAL=${PROJECT} q bank balances ${DEL_ADDR} --output json | jq -r '.balances[] | select(.denom=="'${DENOM}'")' | jq -r .amount)
         BAL=$(($BAL- 500))
 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${DENOM}\n"
         echo -e "Stake ALL\n"
 if (( BAL > 500 )); then
-            echo -e "${PWDDD}\n${PWDDD}\n" | planqd tx staking delegate plqvaloper1923gyajfkyrt7xwh0p7zmfrdnnfrx6gmufltxr ${BAL}${DENOM} --from jb --chain-id planq_7070-2 --gas-adjustment 1.15 --gas=1000000 --gas-prices=30000000000aplanq 
+            echo -e "${PWDDD}\n${PWDDD}\n" | ${PROJECT} tx staking delegate ${VAL_ADDR} ${BAL}${DENOM} --from ${WALLETNAME} --chain-id ${CHAIN_ID} --gas-adjustment ${GAS_AD} --gas=${GAS} --gas-prices=${GAS_PRICE} 
  
         else
                                 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} $DENOM BAL < 0 ((((\n"
